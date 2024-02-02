e3e8813d8af817a6dbcfa20d02bd9e9e6f4f5f2a
Matheus Catarino França <matheus-catarino@hotmail.com>
2024-02-02 13:49:02 -0300

    no_glue

--- src/sokol/c/sokol_imgui.c
+++ src/sokol/c/sokol_imgui.c
@@ -4,7 +4,6 @@
 #include "sokol_app.h"
 #include "sokol_gfx.h"
 #include "sokol_log.h"
-#include "sokol_glue.h"
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include <cimgui.h>
 #include "sokol_imgui.h"
